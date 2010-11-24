d8642f1aef679ae57db133269d57c9b385764b54
Giacomo Ritucci <giacomo.ritucci@email.it>
2010-11-24 23:47:23 +0100

    models: Article and Revision

--- wikisite/toosimplewiki/models.py
+++ wikisite/toosimplewiki/models.py
@@ -1,3 +1,18 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+	title = models.CharField(max_length=200, unique=True)
+
+	def __unicode__ (self):
+		return self.title
+
+
+class Revision(models.Model):
+	article = models.ForeignKey(Article, related_name="revisions")
+	timestamp = models.DateTimeField(primary_key=True, default=datetime.now())
+	content = models.TextField()
+
+	def __unicode__ (self):
+		return "%s %s" % (self.timestamp.strftime("%s"), self.content)
