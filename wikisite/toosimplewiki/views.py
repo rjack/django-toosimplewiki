2ebf194b6458655af508cd12fbce7810c84eefd0
Giacomo Ritucci <giacomo.ritucci@email.it>
2010-11-28 19:58:28 +0100

    article_detail view uses Article.objects.get(pk=...)

--- wikisite/toosimplewiki/views.py
+++ wikisite/toosimplewiki/views.py
@@ -1,11 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response
 from toosimplewiki.models import Article, Revision
 
 
 def article_detail(request, article_id):
-	article = Article.objects.filter(pk=article_id)[0]
-	revision = article.revisions.all().order_by("-timestamp")[0]
+	try:
+		article = Article.objects.get(pk=article_id)
+		revision = article.revisions.all().order_by("-timestamp")[0]
+	except Article.DoesNotExist:
+		raise Http404
 	return render_to_response("toosimplewiki/article_detail.html", {
 		"article": article,
 		"revision": revision,
